2ca178a11c8d2e0df0d207ff1aa378a5c3dc1f81
Gabriela Mercedes Rojas Diaz <rojasgabriela@Gabrielas-MBP.home>
2021-07-11 17:48:31 +0200

    new API link

--- public/javascripts/geo4kids.js
+++ public/javascripts/geo4kids.js
@@ -1,8 +1,20 @@
+import axios from 'axios';
 
 let state = {
   score: 0,
   wrongAnswers: 0
 }
+
+async function getCountries() {
+  try {
+    const response = await axios.get(__dirname + 'src/routes/api');
+    console.log(response)
+  } catch (err) {
+    console.log(err.message);
+  }
+}
+
+getCountries();
 
 function getRandomCountry(countriesArray) {
   let randNum = Math.floor(Math.random() * countriesArray.length);
--- src/routes/api.js
+++ src/routes/api.js
@@ -4,16 +4,16 @@ const fetch = require('node-fetch');
 
 router.get('', async(req, res) => {
     const countriesAPI = await fetchCountries();
-    // console.dir(countriesAPI, { depth: null })
+    console.dir(countriesAPI, { depth: null })
     res.json({ countries : countriesAPI });
 })
 
 const fetchCountries  = async () => {
-    const response = await fetch('https://restcountries.herokuapp.com/api/v1');
+    const response = await fetch('https://restcountries.eu/rest/v2/all');
     const countriesJson = await response.json();
   
     return  countriesJson.map((country) => {
-      return { name: country.name.common, capital: country.capital }
+      return { name: country.name, capital: country.capital }
     })
 }
 
